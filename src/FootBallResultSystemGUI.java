8ce291e01ec584f56a586e0aad27b0e1b64b255d
MaralFarahmandfar <maralfarahmandfar1111@gmail.com>
2024-01-08 22:15:10 +0330

    start frontend

--- src/FootBallResultSystemGUI.java
+++ src/FootBallResultSystemGUI.java
@@ -1,4 +1,20 @@
+import java.awt.event.ActionListener;
 
-public class FootBallResultSystemGUI {
+import javax.swing.JFrame;
+
+public class FootBallResultSystemGUI extends JFrame implements ActionListener {
+	public FootBallResultSystemGUI() {
+		super("FootBall Result System");
+		setSize(750,500);
+		setLayout(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		addGUIComponents();
+	}
+
+	private void addGUIComponents() {
+		
+		
+	}
 
 }
